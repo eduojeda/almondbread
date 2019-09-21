afa4d753309bb2f3c0f3a548079cd3227cc1c597
Eduardo Ojeda <eduojeda@gmail.com>
2019-09-21 18:49:11 +0200

    Rearranged VAO/VBO calls for clarity

--- src/fractal_renderer.cpp
+++ src/fractal_renderer.cpp
@@ -58,17 +58,18 @@ void FractalRenderer::initializeScreenQuad() {
     };
 
     glGenVertexArrays(1, &VAO_);
-    glGenBuffers(1, &VBO_);
-    glGenBuffers(1, &EBO_);
     glBindVertexArray(VAO_);
+
+    // vertex shader attribute: aPos
+    glGenBuffers(1, &VBO_);
     glBindBuffer(GL_ARRAY_BUFFER, VBO_);
     glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
-
-    // positions
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
     glEnableVertexAttribArray(0);
+
+    glGenBuffers(1, &EBO_);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
