298b8f1c89f4645edf271f789da939236eb5c51d
Eduardo Ojeda <eduojeda@gmail.com>
2019-09-23 18:49:09 +0200

    Implemented burning ship fractal

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -15,8 +15,8 @@ void main() {
     double re = start.x + delta.x * gl_FragCoord.x;
     double im = start.y + delta.y * gl_FragCoord.y;
 
-    //int iterations = mandelbrot(0.0, 0.0, re, im, maxIterations); // Mandelbrot
-    int iterations = mandelbrot(re, im, cursorCoords.x, cursorCoords.y, maxIterations); // Julia
+    int iterations = mandelbrot(0.0, 0.0, re, im, maxIterations); // Mandelbrot
+    //int iterations = mandelbrot(re, im, cursorCoords.x, cursorCoords.y, maxIterations); // Julia
 
     fragColor = getColor(iterations, maxIterations);
 };
@@ -45,6 +45,26 @@ int mandelbrot(double zRe, double zIm, double cRe, double cIm, int maxIter) {
         zIm += zIm; // same as x2, multiplication is expensive
         zIm += cIm;
         zRe = zReSq - zImSq + cRe;
+    }
+
+    return maxIter;
+}
+
+int burningShip(double zRe, double zIm, double cRe, double cIm, int maxIter) {
+    double zReSq = 0.0;
+    double zImSq = 0.0;
+
+    for (int iter = 0 ; iter <= maxIter ; iter++) {
+        zReSq = zRe * zRe;
+        zImSq = zIm * zIm;
+        if (zReSq + zImSq >= 4.0) {
+            return iter;
+        }
+
+        zIm = zRe * zIm;
+        zIm += zIm; // same as x2, multiplication is expensive
+        zIm = abs(zIm + cIm);
+        zRe = abs(zReSq - zImSq + cRe);
     }
 
     return maxIter;
